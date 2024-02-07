dd61b69c34a1b4d7b66e56740518486b46142174
gachaupg <gachaupg607@gmail.com>
2024-02-07 15:47:46 +0300

    44

--- src/pages/hero.jsx
+++ src/pages/hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
   return (
     <div className="flex items-center w-full justify-around hero p-3  flex-wrap ">
         <div className="hero-left flex flex-col gap-16"> 
-            <h1 className="text-6xl text-slate-600 font-bold">Welcome to Top PlanCare</h1>
+            <h1 className="text-6xl about-text text-slate-600 font-bold">Welcome to Top PlanCare</h1>
             <p className="text-2xl">We provide the best services in the UK</p>
             <p>Hospitality services play a profound role in palliative care. Hospitality services contribute to dignity, compassion and respect. 
                 A service ecosystems lens is a useful means of organising knowledge. 
