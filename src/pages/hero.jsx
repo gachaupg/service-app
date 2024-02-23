b0f9ebe2302243e6e5fa4489b1cdf33581080cde
gachaupg <gachaupg607@gmail.com>
2024-02-23 08:44:05 +0300

    vvv

--- src/pages/hero.jsx
+++ src/pages/hero.jsx
@@ -11,7 +11,7 @@ const Hero = () => {
                 <p className="text-2xl">We provide the best services in the UK</p>
                 <p className='text-lg text-slate-800 '>We’ll work with you and your family to establish your needs and preferences, creating a
                     bespoke care package that’s completely
-                    unique to you, from occasional visiting care to 24-hour live-in care.!
+                    unique to you, from occasional visiting care to 24-hour live-in care.
                     We provides constant, round-the-clock care from a dedicated carer who lives with you in your home, offering a helping hand at a moment’s notice.
                 </p>
                 <Link to='/contact'>
@@ -23,7 +23,7 @@ const Hero = () => {
 
             </div>
             <div style={{width:'45%'}} className=" leee">
-                <img className="w-full rounded-lg ob" src="https://www.keithrn.com/nurse/wp-content/uploads/2013/12/teaching-caring.jpg" alt="" />
+                <img className="w-full rounded-lg ob" src="https://www.keithrn.com/nurse/wp-content/uploads/2013/12/teaching-caring.jpg" alt="pic" />
 
             </div>
           
